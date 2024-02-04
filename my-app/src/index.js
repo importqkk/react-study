b2bb99945965ccf3dea441fee297564b465d337d
importqkk <import_qkk@naver.com>
2024-02-05 01:08:39 +0900

    component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import Library from './chatper_03/Library';
-import Clock from './chapter_04/Clock';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-setInterval(() => {
-  root.render(
-    <React.StrictMode>
-      <Clock />
-    </React.StrictMode>
-  );
-}, 1000);
+// import Library from './chatper_03/Library';
+// import Clock from './chapter_04/Clock'
+import CommentList from './chapter_05/CommentList';
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <CommentList />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
